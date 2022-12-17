60bf1b770cb819183b01a63beb03b256394b0f58
loleandote <48246750+loleandote@users.noreply.github.com>
2022-12-18 00:46:49 +0100

    Implementacion Camino corto

--- RelacionesSuperheroes/src/Principal.java
+++ RelacionesSuperheroes/src/Principal.java
@@ -1,19 +1,17 @@
-import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
+
 import Grafo.*;
+
 public class Principal {
     public static void main(String[] args) throws Exception {
-        
+
         // Cargando los datos del archivo e imprimiendo el ID de cada vértice.
-       /* Graph<Personaje,Integer> ge=cargarDatosPruebas();
-        mostarMayor(ge);
-        mostarMenor(ge);*/
-        //ge.getVertices().forEachRemaining(v->System.out.println((((Vertex<Personaje>)v).getElement()).getID()));
-        Graph<Personaje,Integer> g=Cargar();
+        Graph<Personaje, Integer> ge = cargarDatosPruebas();
+        // Graph<Personaje,Integer> g=Cargar();
         Scanner sc = new Scanner(System.in);
-        Menu(sc, g);
+        Menu(sc, ge);
         sc.close();
     }
 
@@ -42,7 +40,14 @@ public class Principal {
                     Menu(sc, g);
                     break;
                 case 2:
-                    System.out.println(opcion);
+                    // System.out.println(opcion);
+                    List<Vertex<Personaje>> vertices = iterableToList(g.getVertices());
+                    vertices.forEach(a -> System.out.println(a.getID()));
+                    sc = new Scanner(System.in);
+                    Vertex<Personaje> inicio = obtenerPersonaje(vertices, sc);
+                    Vertex<Personaje> fin = obtenerPersonaje(vertices, sc);
+
+                    camino(g, inicio, fin);
                     Menu(sc, g);
                     break;
                 case 3:
@@ -63,26 +68,83 @@ public class Principal {
         }
     }
 
+    private static Vertex<Personaje> obtenerPersonaje(List<Vertex<Personaje>> vertices, Scanner sc) {
+        // sc.next();
+        String nombre = leerTexto(sc, "Elige un personaje:");
+        if (nombre.equals("") || vertices.stream().filter(a -> a.getID().equals(nombre)).count() == 0)
+            return obtenerPersonaje(vertices, sc);
+        else
+            return vertices.stream().filter(a -> a.getID().equals(nombre)).findFirst().get();
+    }
+
     /**
-     * Crea un nuevo gráfico, imprime el número de vértices y aristas, y luego devuelve el gráfico
+     * Crea un nuevo gráfico, imprime el número de vértices y aristas, y luego
+     * devuelve el gráfico
      * 
      * @return Un gráfico con los vértices y las aristas del gráfico.
      */
     private static Graph<Personaje, Integer> Cargar() {
         Graph<Personaje, Integer> g = new TreeMapGraph<>();
-        
+
         return g;
     }
 
-    private static void MostrarDatos(Graph<Personaje, Integer> g){
-        System.out.println("Numero de personajes:"+g.getN());
-        System.out.println("Numeros de relaciones:"+g.getM());
+    /**
+     * Leer fichero. Este método lee el archivo y crea un gráfico con los nombres
+     * como vértice y el peso.
+     */
+    /*
+     * public static void leerFichero() {
+     * String linea;
+     * boolean seguir = true;
+     * Scanner entrada = null;
+     * Personaje p1;
+     * Personaje p2;
+     * Interaccion i1;
+     * 
+     * try {
+     * entrada = new Scanner(new FileReader("marvel-unimodal-edges.csv"));
+     * } catch(FileNotFoundException fne) {
+     * System.out.println("No se ha podido leer el archivo");
+     * seguir = false;
+     * }
+     * 
+     * entrada.nextLine(); // Salta la primera línea
+     * if(seguir) {
+     * while(entrada.hasNextLine()) {
+     * linea = entrada.nextLine();
+     * StringTokenizer to = new StringTokenizer(linea, ",");
+     * p1 = new Personaje<String>(to.nextToken());
+     * p2 = new Personaje<String>(to.nextToken());
+     * i1 = new Interaccion<Integer>(p1.getID()+"_"+p2.getID(),
+     * Integer.parseInt(to.nextToken()));
+     * DecoratedElement<Personaje<String>> d1 = new
+     * DecoratedElement<Personaje<String>>(p1.getID(), p1);
+     * DecoratedElement<Personaje<String>> d2 = new
+     * DecoratedElement<Personaje<String>>(p2.getID(), p2);
+     * 
+     * t.insertEdge(d1, d2, i1);
+     * 
+     * if(to.hasMoreTokens())
+     * to.nextToken();
+     * 
+     * }
+     * entrada.close();
+     * }
+     * 
+     * }
+     */
+
+    private static void MostrarDatos(Graph<Personaje, Integer> g) {
+        System.out.println("Numero de personajes:" + g.getN());
+        System.out.println("Numeros de relaciones:" + g.getM());
         mostarMayor(g);
         mostarMenor(g);
     }
 
     /**
-     * > Esta función toma un gráfico como entrada e imprime el nombre del Personaje con
+     * > Esta función toma un gráfico como entrada e imprime el nombre del Personaje
+     * con
      * más conexiones
      * 
      * @param g El gráfo a analizar.
@@ -90,12 +152,11 @@ public class Principal {
     private static void mostarMayor(Graph<Personaje, Integer> g) {
         List<Vertex<Personaje>> actualList = iterableToList(g.getVertices());
         try {
-            Vertex<Personaje> c = Collections.max(actualList, (((a,b) ->
-                    (iterableToList2(g.incidentEdges(a)).size() - iterableToList2(g.incidentEdges(b)).size()))));
-            System.out.println("El personaje mas socialble es:");
+            Vertex<Personaje> c = Collections.max(actualList, (((a,
+                    b) -> (iterableToList2(g.incidentEdges(a)).size() - iterableToList2(g.incidentEdges(b)).size()))));
+            System.out.println("El personaje mas sociable es:");
             actualList.forEach((a) -> {
-                if (iterableToList2(g.incidentEdges(a)).size() ==
-                iterableToList2(g.incidentEdges(c)).size())
+                if (iterableToList2(g.incidentEdges(a)).size() == iterableToList2(g.incidentEdges(c)).size())
                     System.out.println(a.getElement().getID());
                 else
                     return;
@@ -104,6 +165,7 @@ public class Principal {
             System.out.println("NO hay elementos en el grafo");
         }
     }
+
     /**
      * > Esta función toma un gráfico e imprime el nombre del Personaje con el menor
      * número de conexiones
@@ -115,12 +177,11 @@ public class Principal {
         List<Vertex<Personaje>> actualList = StreamSupport.stream((iterable).spliterator(), false)
                 .collect(Collectors.toList());
         try {
-            Vertex<Personaje> c = Collections.min(actualList, ((a,b) ->
-                    iterableToList2(g.incidentEdges(a)).size() - iterableToList2(g.incidentEdges(b)).size()));
+            Vertex<Personaje> c = Collections.min(actualList, ((a, b) -> iterableToList2(g.incidentEdges(a)).size()
+                    - iterableToList2(g.incidentEdges(b)).size()));
             System.out.println("El personaje menos socialble es:");
             actualList.forEach((a) -> {
-                if (iterableToList2(g.incidentEdges(a)).size() ==
-                iterableToList2(g.incidentEdges(c)).size())
+                if (iterableToList2(g.incidentEdges(a)).size() == iterableToList2(g.incidentEdges(c)).size())
                     System.out.println(a.getElement().getID());
                 else
                     return;
@@ -131,14 +192,71 @@ public class Principal {
     }
 
     /**
-     * Muestra el camnio mas corto entre dos vertices(personajes introducidos por el
-     * usuario) del grafo
+     * Toma un gráfico, un vértice inicial y un vértice final, e imprime la ruta más
+     * corta desde el
+     * vértice inicial hasta el vértice final.
      * 
-     * @param grafo
-     * @param sc
+     * @param graph  La gráfica
+     * @param inicio El vértice inicial
+     * @param fin    El vértice al que queremos llegar
      */
-    private static void Camino(Graph grafo, Scanner sc) {
+    private static void camino(Graph<Personaje, Integer> graph, Vertex<Personaje> inicio, Vertex<Personaje> fin) {
+        LinkedList<Vertex<Personaje>> queue = new LinkedList<Vertex<Personaje>>();
+        List<Vertex<Personaje>> verticesTotales = iterableToList(graph.getVertices());
+        Personaje[] camino = new Personaje[graph.getN()];
+        Personaje ultimo = inicio.getElement();
+        int[] distancias = new int[graph.getN()];
+        int distancia = 0;
+        for (int i = 0; i < camino.length; i++)
+            camino[i] = null;
+        queue.add(inicio);
+        boolean acabar = false;
+        distancias[verticesTotales.indexOf(inicio)] = 0;
+        do {
+            Vertex<Personaje> v = queue.poll();
+            Iterator<Edge<Integer>> vertices = graph.incidentEdges(v);
+            while (vertices.hasNext() && !acabar) {
+                Vertex<Personaje> adVertex = graph.opposite(v, vertices.next());
+                try {
+                    acabar = adVertex.equals(fin);
+                    if (camino[verticesTotales.indexOf(adVertex)] == null) {
+                        queue.add(adVertex);
+                        ultimo = adVertex.getElement();
+                        distancias[verticesTotales
+                                .indexOf(adVertex)] = distancias[verticesTotales.indexOf(v)] + 1;
+                        distancia = distancias[verticesTotales.indexOf(v)] + 1;
+                        camino[verticesTotales.indexOf(adVertex)] = v.getElement();
+                    }
+                } catch (NullPointerException e) {
+                }
+            }
+        } while (camino[verticesTotales.indexOf(fin)] == null && !queue.isEmpty() && !acabar);
+        if (acabar) {
+            System.out.printf("El camino más corto con una distacia de %d es:\n", distancia);
+            recorreCamino(graph, camino, inicio.getElement(), ultimo);
+            System.out.println(fin.getID());
+        } else
+            System.out.println("no se pudo el ultimo personaje visitado es " + ultimo.getID());
+    }
 
+    /**
+     * Imprime recursivamente la ruta desde el primer vértice hasta el penúltimo
+     * vértice.
+     * 
+     * @param graph   La gráfica
+     * @param camino  La matriz de objetos Personaje que contiene la ruta.
+     * @param primero El primer vértice del camino.
+     * @param vertice El vértice al que queremos encontrar el camino más corto.
+     */
+    private static void recorreCamino(Graph<Personaje, Integer> graph, Personaje[] camino, Personaje primero,
+            Personaje vertice) {
+
+        Personaje vertice2 = camino[iterableToList(graph.getVertices())
+                .indexOf(iterableToList(graph.getVertices()).stream()
+                        .filter(e -> e.getID().equals(vertice.getID())).findFirst().get())];
+        if (!vertice2.getID().equals(primero.getID()))
+            recorreCamino(graph, camino, primero, vertice2);
+        System.out.print(vertice2.getID() + " ");
     }
 
     /**
@@ -187,64 +305,24 @@ public class Principal {
 
     // #endregion
 
-    //#region pruebas
-    private static Graph<Personaje,Integer> cargarDatosPruebas (){
-        Graph<Personaje,Integer> grafo =new TreeMapGraph<Personaje,Integer>();
-        Personaje[]personajes= new Personaje[10];
-        for(int i=0;i<10;i++)
-            personajes[i]=new Personaje(""+(char)('a'+i));
-        grafo.insertEdge(personajes[0],personajes[1], 2);
-        grafo.insertEdge(personajes[0],personajes[2], 8);
-        grafo.insertEdge(personajes[0],personajes[3], 24);
-        grafo.insertEdge(personajes[0],personajes[4], 12);
-        grafo.insertEdge(personajes[2],personajes[5], 6);
-        grafo.insertEdge(personajes[4],personajes[5], 4);
-        grafo.insertEdge(personajes[5],personajes[6], 3);
-        grafo.insertEdge(personajes[6],personajes[7], 5);
-        grafo.insertEdge(personajes[6],personajes[8], 35);
-        grafo.insertEdge(personajes[8],personajes[9], 7);
+    // #region pruebas
+    private static Graph<Personaje, Integer> cargarDatosPruebas() {
+        Graph<Personaje, Integer> grafo = new TreeMapGraph<Personaje, Integer>();
+        Personaje[] personajes = new Personaje[10];
+        Personaje pers = new Personaje("l");
+        for (int i = 0; i < 10; i++)
+            personajes[i] = new Personaje("" + (char) ('a' + i));
+        grafo.insertEdge(personajes[0], personajes[1], 2);
+        grafo.insertEdge(personajes[0], personajes[2], 8);
+        grafo.insertEdge(personajes[0], personajes[3], 24);
+        grafo.insertEdge(personajes[0], personajes[4], 12);
+        grafo.insertEdge(personajes[2], personajes[5], 6);
+        grafo.insertEdge(personajes[4], personajes[5], 4);
+        grafo.insertEdge(personajes[5], personajes[6], 3);
+        grafo.insertEdge(personajes[6], personajes[7], 5);
+        grafo.insertEdge(personajes[6], personajes[8], 35);
+        grafo.insertEdge(personajes[8], personajes[9], 7);
         return grafo;
     }
-//#endregion
+    // #endregion
 }
-     private static TreeMapGraph<DecoratedElement<Personaje<String>>, Interaccion<Integer>> t = new TreeMapGraph();
-	
-	/**
-	 * Leer fichero. Este método lee el archivo y crea un gráfico con los nombres como vértice y el peso.
-	 */
-	    public static void leerFichero() {
-		String linea;
-		boolean seguir = true;
-		Scanner entrada = null;
-		Personaje p1;
-		Personaje p2;
-		Interaccion i1;
-		
-		try {
-			entrada = new Scanner(new FileReader("marvel-unimodal-edges.csv"));
-		} catch(FileNotFoundException fne) {
-			System.out.println("No se ha podido leer el archivo");
-			seguir = false;
-		}
-		
-		entrada.nextLine(); // Salta la primera línea
-		if(seguir) {
-			while(entrada.hasNextLine()) {
-				linea = entrada.nextLine();
-				StringTokenizer to = new StringTokenizer(linea, ",");
-				p1 = new Personaje<String>(to.nextToken());
-				p2 = new Personaje<String>(to.nextToken());
-				i1 = new Interaccion<Integer>(p1.getID()+"_"+p2.getID(), Integer.parseInt(to.nextToken()));
-				DecoratedElement<Personaje<String>> d1 = new DecoratedElement<Personaje<String>>(p1.getID(), p1);
-				DecoratedElement<Personaje<String>> d2 = new DecoratedElement<Personaje<String>>(p2.getID(), p2);
-				
-				t.insertEdge(d1, d2, i1);
-				
-				if(to.hasMoreTokens())
-					to.nextToken();
-				
-			}
-			entrada.close();
-		}
-		
-	}
